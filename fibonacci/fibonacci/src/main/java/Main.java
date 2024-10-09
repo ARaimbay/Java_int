9262fcca90587f649074959deb984a08c49e280a
Aigerim Raimbay <raimbay89@gmail.com>
2024-10-09 14:14:10 -0500

    fibonacci nth value solution

--- fibonacci/fibonacci/src/main/java/Main.java
+++ fibonacci/fibonacci/src/main/java/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and Welcome");
+
+        for (int i = 1; i <= 100; i++) {
+            int fib = fibonacci(i);
+            System.out.println("Fib i = " + i + " = " + fib);
+        }
+
+    }
 }
